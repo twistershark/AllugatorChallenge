569656fa0c50975080fc036366fdb87ddcaaf064
Paulo Victor da Silva <paulovmel@gmail.com>
2020-11-15 12:59:37 -0300

    feat: routes validation

--- backend/src/modules/collaborators/infra/http/routes/collaborators.routes.ts
+++ backend/src/modules/collaborators/infra/http/routes/collaborators.routes.ts
@@ -15,25 +15,44 @@ const listCollaboratorsController = new ListCollaboratorsController();
 
 // Create, Read, Update and Delete Collaborator
 
-
 collaboratorsRouter.get('/', collaboratorsController.index);
-collaboratorsRouter.post('/', celebrate({
-  [Segments.BODY]: {
-    name: Joi.string().required(),
-    cpf: Joi.string().required().length(11),
-    job: Joi.string().required(),
-    signUpDate: Joi.string().required().length(10),
-    uf: Joi.string().required().length(2),
-    salary: Joi.number().required(),
-    status: Joi.string().required(),
-  }
-}),collaboratorsController.create);
-collaboratorsRouter.delete('/:cpf', collaboratorsController.delete);
+collaboratorsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      cpf: Joi.string().required().length(11),
+      job: Joi.string().required(),
+      signUpDate: Joi.string().required().length(10),
+      uf: Joi.string().required().length(2),
+      salary: Joi.number().required(),
+      status: Joi.string().required(),
+    },
+  }),
+  collaboratorsController.create,
+);
+collaboratorsRouter.delete(
+  '/:cpf',
+  celebrate({
+    [Segments.PARAMS]: {
+      cpf: Joi.string().required().length(11),
+    },
+  }),
+  collaboratorsController.delete,
+);
 
 // List collaborators by query params
 collaboratorsRouter.get('/list', listCollaboratorsController.index);
 
 // Show collaborator using CPF
-collaboratorsRouter.get('/:cpf', showCollaboratorByCPFController.index);
+collaboratorsRouter.get(
+  '/:cpf',
+  celebrate({
+    [Segments.PARAMS]: {
+      cpf: Joi.string().required().length(11),
+    },
+  }),
+  showCollaboratorByCPFController.index,
+);
 
 export default collaboratorsRouter;
